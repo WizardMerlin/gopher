5b7b4a04dbe808a7bcc859f65793656305a1659c
WizardMerlin <yuyankai945@hotmail.com>
2018-03-17 12:52:11 +0800

    update html_template complex concurrent

--- cases/html_templates/complex_concurret.go
+++ cases/html_templates/complex_concurret.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"sync"
 	"log"
 	"encoding/xml"
 	"fmt"
@@ -29,6 +30,8 @@ type NewsPara struct {
 	News  map[string]NewsMap
 }
 
+var wg sync.WaitGroup
+
 func main() {
 	http.HandleFunc("/news/", newsHandler)
 	http.ListenAndServe(":8080", nil)
@@ -37,7 +40,7 @@ func main() {
 func newsHandler(w http.ResponseWriter, r *http.Request) {
 
 	var s Sitemapindex
-	var n News
+
 
 	//获取 xml 信息
 	resp, err := http.Get("https://www.washingtonpost.com/news-sitemap-index.xml")
@@ -56,40 +59,38 @@ func newsHandler(w http.ResponseWriter, r *http.Request) {
 		log.Fatal("get the news xml locations error")
 	}
 
-	//调试用
-/* 	for url := range s.Locations {
-		log.Println(url)
-		fmt.Println("s", url)
-	} */
-
-	//解析 xml
-	newsMap := make(map[string]NewsMap) //存储单个每一条新闻
+	queue := make(chan News, 50) //缓冲队列有50，然后开启100个 goroutines
+	log.Println(len(s.Locations)) //22
 	for _, Location := range s.Locations {
-		resp, _ := http.Get(Location)
-		bytes, _ := ioutil.ReadAll(resp.Body)
-		resp.Body.Close()
-		xml.Unmarshal(bytes, &n) //拿到具体新闻 News
+		wg.Add(1)
+		go worker(queue, Location) //往 News 队列里面写拿到的 xml 具体信息
+	}
 
-		//这里应该检查一下 n
-		if len(n.Keywords)==0 {
-			log.Fatal("get the news xml content error")
+	wg.Wait()
+	close(queue)
+
+
+	// 从 chan 中拿到 News 写入 map
+	newsMap := make(map[string]NewsMap) //存储单个每一条新闻
+	for elem := range queue {
+		for idx := range elem.Keywords {
+			newsMap[elem.Titles[idx]] = NewsMap{elem.Keywords[idx], elem.Locations[idx]}
 		}
-
-		//存储到 NewsMap中
-		for idx := range n.Keywords {
-			newsMap[n.Titles[idx]] = NewsMap{n.Keywords[idx], n.Locations[idx]}
-		}
-
-		//调试用
-/* 		for kk, vv := range newsMap {
-			log.Printf("title: %s\n", kk)
-			log.Printf("News: keywords {%s} ", vv.Keyword)
-		} */
-
 	}
 
 	p := NewsPara{Title: "Wow", News: newsMap}
 	t, _ := template.ParseFiles("complex.html")
 	//t.Execute(w, p)
 	fmt.Println(t.Execute(w, p)) //在控制台打印消息 (解析错误会有报错)
+}
+
+func worker(newsChan chan News, Location string) {
+	defer wg.Done()
+	var n News
+	resp, _ := http.Get(Location) //1
+	bytes, _ := ioutil.ReadAll(resp.Body) //2
+	resp.Body.Close()
+	xml.Unmarshal(bytes, &n) //拿到具体新闻 News
+
+	newsChan <- n
 }
